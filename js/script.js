a9668eef3b75c1cd48a0e38074a5d9a5d2ea0ece
Belloaise Carrington <Bello@anniecanonssMBP.hsd1.ga.comcast.net>
2024-06-21 17:31:30 -0400

    added toggle nav bar on certain screen size

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ function onFormSubmit(event) {
 	console.log(userArray);
 	form.reset();
 }
+
+
+const burger = document.querySelector(".burger i");
+console.log(burger);
+const nav = document.querySelector(".nav");
+console.log(nav);
+
+function showHamburger() {
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active");
+}

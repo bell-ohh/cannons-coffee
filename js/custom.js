d1896b3b4f5bb6331f727ed8efd17a7bf3c3cabc
Belloaise Carrington <Bello@anniecanonssMBP.hsd1.ga.comcast.net>
2024-06-28 16:13:45 -0400

    fixed toggle navbar

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
+
+//toggle
+const burger = document.querySelector(".burger i");
+console.log(burger);
+const nav = document.querySelector(".nav");
+console.log(nav);
+
+function showHamburger() {
+	burger.classList.toggle("fa-times");
+	nav.classList.toggle("nav-active");
+	console.log("clicked");
+}
+
+//form button
+
 const form = document.getElementById("form");
 let userArray = [];
 
